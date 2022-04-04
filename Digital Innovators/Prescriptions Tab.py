3cd3c2f619ac225a9b412db774ea88390878dc54
Yusuf <yusufseyyed04@gmail.com>
2022-04-04 15:42:51 +0100

    updating

--- Digital Innovators/Prescriptions Tab.py
+++ Digital Innovators/Prescriptions Tab.py
@@ -16,27 +16,33 @@ var1 = StringVar()
 listbox = Listbox(window, width= 100, height=46, bg="white")
 listbox.place(x=50,y=155,width=500,height=400)
 
-firstname = "Daryle"
-sirname = "Playle"
+#for index, row in dataframe.iterrows():
+    #print(index, row["med1"])
 
-name = "Showing medication for " + firstname + " " + sirname
+#first_name = dataframe.loc("first_name")
+
+#meds = StringVar
+#meds = dataframe.loc[dataframe["med1"].str.contains(first_name, flags=re.I, regex=True)]
+#print(meds.to_string)
+
+firstname = "Daryle"
+surname = "Playle"
+
+name = "Showing medication for " + firstname + " " + surname
 nameLabel = Label(window, bg='cyan4', fg='white', font=("Verdana", 17))
 nameLabel.place(x=50,y=50)
 nameLabel.config(text=name)
-scrollbar = Scrollbar
 
-def search():
-    pass
-    #specific_person = dataframe.loc[dataframe["first_name"] + dataframe["last_name"] == firstname.get()= lastname.get()]
-    #id = specific_person.index
-    #print(id)
-    #print(specific_person)
+searching = dataframe.loc[dataframe["first_name"] + dataframe["last_name"] == firstname + surname]  # change firstname surname .get()
+index = searching.index
 
-def clickevent(self):
-    val = listbox.get(listbox.curselection)
-    meds = dataframe.loc[dataframe["med1"] == val] #finds the data on the row
-    print(val)
+Medication = dataframe["med1"] #seperate medication from each other and find out how to specify items in csv are lists
 
-listbox.bind('<<ListboxSelect>>', clickevent)
+for med in Medication[index].item():
+    if len(med) >= 1:
+        listbox.insert("end", med)
+    else:
+        listbox.insert("end", med)
+
 
 window.mainloop()
