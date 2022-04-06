955c5e877c99fd540d9367ac318fa44f396e6e21
Ibzino <nassaribrahim2004@gmail.com>
2022-04-06 10:37:20 +0100

    finished encryption code does need the 1 taken off the csv in the code tho

---
+++ Digital Innovators/Application_parts/encryption.py
@@ -0,0 +1,40 @@
+from cryptography.fernet import Fernet
+from tkinter import *
+import os
+
+def key_load(key_name):
+    with open(key_name, 'rb') as myKey:
+        key = myKey.read()
+    return key
+
+def file_encrypt(orignial_file):
+    key = key_load('myKey.key')
+    f = Fernet(key)
+    try:
+        with open(orignial_file, 'rb') as file:
+            original = file.read()
+
+        encrypted = f.encrypt(original)
+
+        with open(orignial_file, 'wb') as file:
+            file.write(encrypted)
+    except FileNotFoundError:
+        print("Sorry File Not Found!")
+
+def file_decrypt(original_file):
+    key = key_load('myKey.key')
+    f = Fernet(key)
+
+    try:
+        with open(original_file, 'rb') as file:
+            encrypted = file.read()
+
+        decrypted = f.decrypt(encrypted)
+
+        with open(original_file, 'wb') as file:
+            file.write(decrypted)
+    except FileNotFoundError:
+        print("Sorry File Not Found!")
+
+
+file_encrypt("MOCK_DATA1.csv")
